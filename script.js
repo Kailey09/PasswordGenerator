af14828b02bba8475227d196aa4103f43c83b199
Kailey09 <kcoop1323@gmail.com>
2022-02-26 15:21:46 -0500

    added const length and randomLower

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-// Assignment code here
+const length = ""
+const randomLower = "abcdefghijklmnopqrstuvwxyz"
+const randomUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+
+// Assignment code here
+var generatePassword = function() {
+ 
+const length = window.prompt("Pick a number between 8-128 to determine password length.");
+console.log(length);
+
+const randomLower = window.prompt("Would you like to add a lowercase letter? Enter YES or NO.");
+if (randomLower === "YES" || randomLower === "yes") {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+  
+
+
+};
+
+
+
+generatePassword();
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
