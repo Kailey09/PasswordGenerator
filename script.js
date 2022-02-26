3e7135c20b7630990ba0638dbc809bd1b0370b30
Kailey09 <kcoop1323@gmail.com>
2022-02-26 17:45:03 -0500

    added numbers and symbols

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// Assignment code here
 const length = ""
 const randomLower = "abcdefghijklmnopqrstuvwxyz"
 const randomUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const symbols = " !@#$%^&*()"
+const numbers = "1234567890"
 
 
 // Assignment code here
@@ -11,16 +14,28 @@ console.log(length);
 
 const randomLower = window.prompt("Would you like to add a lowercase letter? Enter YES or NO.");
 if (randomLower === "YES" || randomLower === "yes") {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  console.log (String.fromCharCode(Math.floor(Math.random() * 26) + 97));
+}
+
+const randomUpper = window.prompt("Would you like to add a lowercase letter? Enter YES or NO");
+if (randomUpper === "YES" || randomUpper === "yes") {
+  console.log(String.fromCharCode(Math.floor(Math.random()* 26) + 65));
+}
+
+const symbols = window.prompt("Would you like to add a sybmol in your password? Enter YES or NO");
+if (symbols === "YES" || symbols === "yes") {
+  console.log (symbols(Math.floor(Math.random() * symbols.length)));
+}
+
+const numbers = window.prompt("Would you like to add a number in your password? Enter YES or NO");
+if (numbers === "YES" || numbers === "yes") {
+  console.log (String.fromCharCode(Math.floor(Math.random() * 10) + 48));
 }
 
   
 
 
 };
-
-
-
 generatePassword();
 
 // Get references to the #generate element
