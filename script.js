eeee914f30c4bb15bdb23925ee9149932a0f288f
Kailey09 <kcoop1323@gmail.com>
2022-02-27 18:53:24 -0500

    added variables to create random password

--- script.js
+++ script.js
@@ -1,37 +1,50 @@
 // Assignment code here
-const length = ""
-const randomLower = "abcdefghijklmnopqrstuvwxyz"
-const randomUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-//                   012 ..                   25
-const symbols = " !@#$%^&*()"
-const numbers = "1234567890"
+var length = ""
+var randomLower = "abcdefghijklmnopqrstuvwxyz"
+var randomUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var symbols = " !@#$%^&*()"
+var numbers = "1234567890"
 
 
 // Assignment code here
 var generatePassword = function() {
  
 var length = window.prompt("Pick a number between 8-128 to determine password length.");
-var result = "";
-
-for(i = 0; i < length; i++){
-
-  var randomIndex = Math.floor(Math.random() * 26);
-  var randomChar = randomUpper[randomIndex];
-
-  result = result + randomChar;
-}
 
 var includesLowercase = window.confirm("Would you like to add a lowercase letter? Enter YES or NO.");
-
 var includesUppercase = window.confirm("Would you like to add a lowercase letter? Enter YES or NO");
 
 var includesSymbols = window.confirm("Would you like to add a sybmol in your password? Enter YES or NO");
 
 var includesNumbers = window.confirm("Would you like to add a number in your password? Enter YES or NO");
 
+  var result = "" 
+  for(i = 0; i < length; i++){
+    
+    var randomNumber = Math.floor(Math.random()* 10);
+    var randomDigit = numbers[randomNumber];
+   
+   
+    var randomIndex = Math.floor(Math.random() * 26);
+    var randomChar = randomUpper[randomIndex];
+
+    var randomSymbol = Math.floor(Math.random()* 10);
+    var randomThing = symbols[randomSymbol];
+
+    var lilAlpha = Math.floor(Math.random() * 26);
+    var tinyChar = randomLower[lilAlpha];
+    
+  
+    result = result + randomDigit + randomChar + randomThing + tinyChar;
 
 
-return result;
+  }
+  
+  return result;
+
+
+
+
 };
 
 
